fd84a89c5a9c98b7d6dfdeb3ecb7e3afc4c0ea77
FelixJentsch <FelixJentsch91@googlemail.com>
2023-12-06 16:52:17 +0100

    challenge solved

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -2,13 +2,15 @@ import { useEffect, useState } from "react";
 
 export default function PokemonList() {
   const [pokemon, setPokemon] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
     async function loadPokemon() {
       try {
-        const response = await fetch(
-          "https://pokeapi.co/api/v2/pokemon?offset=0"
-        );
+        const offset = (currentPage - 1) * 20;
+        const apiURL = `https://pokeapi.co/api/v2/pokemon?offset=${offset}`;
+
+        const response = await fetch(apiURL);
         const data = await response.json();
         setPokemon(data.results);
       } catch (error) {
@@ -17,12 +19,20 @@ export default function PokemonList() {
     }
 
     loadPokemon();
-  }, []);
+  }, [currentPage]);
 
   return (
     <main>
-      <button type="button">Previous Page</button>
-      <button type="button">Next Page</button>
+      <button
+        type="button"
+        onClick={() => setCurrentPage(currentPage - 1)}
+        disabled={currentPage === 1}
+      >
+        Previous Page
+      </button>
+      <button type="button" onClick={() => setCurrentPage(currentPage + 1)}>
+        Next Page
+      </button>
       <ul>
         {pokemon.map(({ name }) => (
           <li key={name}>{name}</li>
